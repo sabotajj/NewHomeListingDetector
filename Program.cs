2a746722fbccbed210f13a295b37383f2ef1620d
Shabtay Erdemanar <shabtay@cedato.com>
2017-11-06 21:10:03 +0200

    keeping several cache for deleting disseapeared houses.

--- Program.cs
+++ Program.cs
@@ -46,7 +46,7 @@ namespace homefinderYad2
         private bool firstRun = true;
         
         private List<string> parametersCollection=null;
-        private List<homeClass> cache=null;
+        private List<List<homeClass>> cache = null;
         public yad2Layer()
         {
             buildParams();
@@ -57,6 +57,7 @@ namespace homefinderYad2
             //parametersCollection.Add("?SubCatID=2&AreaID=&City=&HomeTypeID=&fromRooms=2.5&untilRooms=3.5&fromPrice=&untilPrice=7500&PriceType=1&FromFloor=1&ToFloor=&fromSquareMeter=65&untilSquareMeter=&EnterDate=&Info=&coords%5Btop%5D%5Blat%5D=32.0812407485499&coords%5Btop%5D%5Blng%5D=34.76860038936138&coords%5Bbottom%5D%5Blat%5D=32.063020925819046&coords%5Bbottom%5D%5Blng%5D=34.76860038936138&coords%5Bright%5D%5Blat%5D=32.07213038336747&coords%5Bright%5D%5Blng%5D=34.77935106571829&coords%5Bleft%5D%5Blat%5D=32.07213038336747&coords%5Bleft%5D%5Blng%5D=34.757849713004475&radius=1012.9759260172019&centerCoords%5Blat%5D=32.072130837184474&centerCoords%5Blng%5D=34.76860038936138&searchMode=radius&_="+ (int)getUnixTimeNow());
             parametersCollection.Add("?SubCatID=2&AreaID=1&City=&HomeTypeID=&fromRooms=2.5&untilRooms=&fromPrice=&untilPrice=8500&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info=&coords%5Btop%5D%5Blat%5D=32.077102311118544&coords%5Btop%5D%5Blng%5D=34.76807611310505&coords%5Bbottom%5D%5Blat%5D=32.06465515128003&coords%5Bbottom%5D%5Blng%5D=34.76807611310505&coords%5Bright%5D%5Blat%5D=32.070878519406136&coords%5Bright%5D%5Blng%5D=34.77542050700049&coords%5Bleft%5D%5Blat%5D=32.070878519406136&coords%5Bleft%5D%5Blng%5D=34.760731719209616&radius=692.0305125887065&centerCoords%5Blat%5D=32.070878731199294&centerCoords%5Blng%5D=34.76807611310505&searchMode=radius&_=" + (int)getUnixTimeNow());
             parametersCollection.Add("?SubCatID=2&AreaID=1&City=&HomeTypeID=&fromRooms=2.5&untilRooms=&fromPrice=&untilPrice=8500&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info=&coords%5Btop%5D%5Blat%5D=32.09144237371608&coords%5Btop%5D%5Blng%5D=34.76313625025796&coords%5Bbottom%5D%5Blat%5D=32.06837841287437&coords%5Bbottom%5D%5Blng%5D=34.76313625025796&coords%5Bright%5D%5Blat%5D=32.07990966586507&coords%5Bright%5D%5Blng%5D=34.7767463869809&coords%5Bleft%5D%5Blat%5D=32.07990966586507&coords%5Bleft%5D%5Blng%5D=34.74952611353501&radius=1282.2977169630556&centerCoords%5Blat%5D=32.07991039329522&centerCoords%5Blng%5D=34.76313625025796&searchMode=radius&_=" + (int)getUnixTimeNow());
+            cache = new List<List<homeClass>>(parametersCollection.Count);
         }
         private double getUnixTimeNow() {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
@@ -83,7 +84,7 @@ namespace homefinderYad2
                 string responseFromServer = reader.ReadToEnd();
                 var houses = parseResponse(responseFromServer);
                 Console.WriteLine(houses.Count + " houses found");
-                var newhouses = returnNewHouses(houses);
+                var newhouses = returnNewHouses(houses, parametersCollection.IndexOf(param));
                 if (newhouses.Count > 0)
                 {
                     Console.WriteLine(newhouses.Count + " new houses found");
@@ -137,38 +138,39 @@ namespace homefinderYad2
             result = null;
             return resultList;
         }
-        private List<homeClass> returnNewHouses(List<homeClass> houses)
+        private List<homeClass> returnNewHouses(List<homeClass> houses,int paramIndex)
         {
             List<homeClass> result = new List<homeClass>();
-            if(cache == null)
+            if(cache[paramIndex] == null)
             {
-                cache = houses;
+
+                cache[paramIndex] = houses;
             }
             else
             {
-                result = houses.Where(x => !cache.Any(y => x.PostNo == y.PostNo)).ToList<homeClass>();
+                result = houses.Where(x => !cache[paramIndex].Any(y => x.PostNo == y.PostNo)).ToList<homeClass>();
                 if (result.Count > 0)
                 {
-                    cache.AddRange(result);
+                    cache[paramIndex].AddRange(result);
                 }
-                var updatedHouses = houses.Where(x => replaceRecordInCacheIfNeed(x)).ToList();
+                var updatedHouses = houses.Where(x => replaceRecordInCacheIfNeed(x,paramIndex)).ToList();
                 if (updatedHouses.Count > 0) {
                     result.AddRange(updatedHouses);
                 }
-                var dissappearedHouses = cache.Where(x => !houses.Any(y => y.PostNo == x.PostNo));
-                dissappearedHouses.ToList().ForEach(dissappearedHouse => cache.Remove(dissappearedHouse));
+                var dissappearedHouses = cache[paramIndex].Where(x => !houses.Any(y => y.PostNo == x.PostNo));
+                dissappearedHouses.ToList().ForEach(dissappearedHouse => cache[paramIndex].Remove(dissappearedHouse));
             }
             return result;
         }
-        private bool replaceRecordInCacheIfNeed(homeClass newHouse)
+        private bool replaceRecordInCacheIfNeed(homeClass newHouse,int paramIndex)
         {
-            var oldHouses = cache.Where(y => (y.PostNo == newHouse.PostNo) && (y.Street != newHouse.Street)).ToList();
+            var oldHouses = cache[paramIndex].Where(y => (y.PostNo == newHouse.PostNo) && (y.Street != newHouse.Street)).ToList();
             
             int oldHouseCount = oldHouses.Count;
             if (oldHouseCount > 0)
             {
-                oldHouses.ForEach(x => cache.Remove(x));
-                cache.Add(newHouse);
+                oldHouses.ForEach(x => cache[paramIndex].Remove(x));
+                cache[paramIndex].Add(newHouse);
             }
             return oldHouseCount > 0;
         }
